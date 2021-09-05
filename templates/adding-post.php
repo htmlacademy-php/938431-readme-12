d1a3c6c42cd0c1dea3312c5a23cbe4ddaa4351f0
Yulia Elagina <julia-elagina@yandex.ru>
2021-09-05 22:27:57 +0300

    Изменяет имена некоторых полей форм

--- templates/adding-post.php
+++ templates/adding-post.php
@@ -153,9 +153,9 @@
                       <input type="hidden" name="post-type" id="post-type" value="quote">
                       <?=$title_field ?>
                       <div class="adding-post__input-wrapper form__textarea-wrapper">
-                        <label class="adding-post__label form__label" for="cite-text">Текст цитаты <span class="form__input-required">*</span></label>
+                        <label class="adding-post__label form__label" for="quote-text">Текст цитаты <span class="form__input-required">*</span></label>
                         <div class="form__input-section">
-                          <textarea class="adding-post__textarea adding-post__textarea--quote form__textarea form__input" id="cite-text" name="cite-text" placeholder="Текст цитаты"></textarea>
+                          <textarea class="adding-post__textarea adding-post__textarea--quote form__textarea form__input" id="quote-text" name="quote-text" placeholder="Текст цитаты"></textarea>
                           <button class="form__error-button button" type="button">!<span class="visually-hidden">Информация об ошибке</span></button>
                           <div class="form__error-text">
                             <h3 class="form__error-title">Заголовок сообщения</h3>
