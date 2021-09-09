f3e7f5028999577d001fbf5b0e973844de806bdb
Yulia Elagina <julia-elagina@yandex.ru>
2021-09-09 18:32:06 +0300

    Вносит мелкие правки в шаблоны

--- templates/field-tags.php
+++ templates/field-tags.php
@@ -1,6 +1,6 @@
 <div class="adding-post__input-wrapper form__input-wrapper">
     <label class="adding-post__label form__label" for="tags"><?=$label; ?></label>
-    <div class="form__input-section <?php if(!empty($error['tags'])) echo 'form__input-section--error' ?>">
+    <div class="form__input-section <?php if($error) echo 'form__input-section--error' ?>">
         <input class="adding-post__input form__input" id="tags" type="text" name="tags" placeholder="Введите теги" value="<?=get_post_value('tags'); ?>">
         <button class="form__error-button button" type="button">!<span class="visually-hidden">Информация об ошибке</span></button>
         <div class="form__error-text">
