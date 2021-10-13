275a4dc60caf8ebdf3322c1c846cac7614e87382
Yulia Elagina <julia-elagina@yandex.ru>
2021-10-13 18:53:51 +0300

    Вносит исправления в адрес фавиконки в шаблон поста для страницы Поста

--- templates/details-link.php
+++ templates/details-link.php
@@ -3,10 +3,10 @@
         <a class="post-link__external" href="<?=$url;?>" title="Перейти по ссылке">
             <div class="post-link__info-wrapper">
                 <div class="post-link__icon-wrapper">
-                    <img src="<?=$url;?>/favicon.ico" alt="Иконка">
+                    <img src="<?=generate_favicon_url($url);?>" alt="Иконка">
                 </div>
                 <div class="post-link__info">
-                    <h3><?=$url;?></h3>
+                    <h3><?=extract_domain_name($url);?></h3>
                 </div>
             </div>
         </a>
