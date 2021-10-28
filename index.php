c0c068712e4b5dfeca40b62cf66bef13082c59ac
Yulia Elagina <julia-elagina@yandex.ru>
2021-10-28 20:14:01 +0300

    Создает функции для получения разметки футера и процесса авторизации

--- index.php
+++ index.php
@@ -14,47 +14,11 @@ require_once('helpers.php');
 $con = set_connection();
 
 $errors = [];
-
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $form = $_POST;
-
-    // Проверяем заполненность обязательных полей
-    foreach ($form as $key => $value) {
-        if (empty($form[$key])) {
-            $errors[$key] = 'Это поле должно быть заполнено';
-        }
-    }
-
-    // Проверяем существование пользователя с введенным email
+    $errors = authorize_user($form, $con);
     if (empty($errors)) {
-        $email = filter_var($form['login'], FILTER_VALIDATE_EMAIL);
-        if ($email) {
-            $sql = "SELECT* FROM user WHERE email = '$email';";
-            $result = mysqli_query($con, $sql);
-            $user = $result ? mysqli_fetch_array($result, MYSQLI_ASSOC) : null;
-            if ($user) {
-                // Проверяем пароль и открываем сессию
-                if (password_verify($form['password'], $user['u_password'])) {
-                    // Создаем запрос на количество непрочитанных сообщений
-                    $user_id = $user['id'];
-                    $sql = "SELECT COUNT(id) AS m_count
-                        FROM message
-                        WHERE receiver_id = '$user_id'
-                        AND is_new = TRUE;";
-                    $result = mysqli_query($con, $sql);
-                    $user['m_count'] = $result ? mysqli_fetch_assoc($result)['m_count'] : 0;
-
-                    $_SESSION['user'] = $user;
-                    header("Location: /feed.php");
-                } else {
-                    $errors['password'] = 'Неверный пароль';
-                }
-            } else {
-                $errors['login'] = 'Пользователь с таким email не найден';
-            }
-        } else {
-            $errors['login'] = 'Введен некорректный email';
-        }
+        header("Location: /feed.php");
     }
 }
 $layout = include_template('main.php', ['errors' => $errors]);
---
+++ login.php
@@ -0,0 +1,51 @@
+<?php
+session_start();
+$user = $_SESSION['user'] ?? null;
+
+// Залогиненных пользователей перенаправляем на страницу Моя лента
+if ($user) {
+    header('Location: /feed.php');
+    exit;
+}
+
+require_once('helpers.php');
+
+// Устанавливаем соединение с базой readme
+$con = set_connection();
+
+$errors = [];
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $form = $_POST;
+    $errors = authorize_user($form, $con);
+    if (empty($errors)) {
+        header("Location: /feed.php");
+    }
+}
+
+$label = [
+    'login' => 'Электронная почта',
+    'password' => 'Пароль',
+];
+
+$invalid_block = '';
+
+if (count($errors)) {
+    $invalid_block = include_template('invalid-block.php', [
+        'errors' => $errors,
+        'label' => $label
+    ]);
+}
+
+$content = include_template('user-login.php', [
+    'errors' => $errors,
+    'invalid_block' => $invalid_block
+]);
+
+$layout = include_template('layout.php', [
+    'page_content' => $content,
+    'page_title' => 'readme: авторизация',
+    'user' => null
+]);
+
+print($layout);
